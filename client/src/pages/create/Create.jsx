3e451f7adba781c170bd48f25deb55d3e08f2a71
rajatHere01 <90706207+rajatHere01@users.noreply.github.com>
2023-07-29 14:13:55 +0530

    Update Create.jsx

--- client/src/pages/create/Create.jsx
+++ client/src/pages/create/Create.jsx
@@ -47,7 +47,7 @@ const Create = () => {
         formData.append("filename", filename)
         formData.append("image", img)
 
-        await fetch(`http://localhost:5000/upload`, {
+        await fetch(`https://blog-website-9lvt.onrender.com/upload`, {
           method: "POST",
           body: formData
         })
